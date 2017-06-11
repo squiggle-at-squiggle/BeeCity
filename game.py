7a8d8ff19d2ce9384025458a16604a131253a4d9
spragglerock <sprasarah@gmail.com>
2017-06-11 14:52:13 -0500

    add game header and main loop

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
-#This is the place where the game is
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+while True: #the game loop
+  #Handle Events
+  for event in pygame.event.get():
+    #Update Game State
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+
+  #Draw The Screen
+  pygame.display.update()
